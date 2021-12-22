f48cd3b66ffcd7848d47f095dd1d4b9031e13b21
haman-pakrah <88421521+haman-pakrah@users.noreply.github.com>
2021-12-22 08:17:42 +0330

    توکنایز کردن
    این کد وقتی صدای کاربر را ورودی گرفت،توکنایز می کند و به صورت یک لیست خروجی می دهد.

---
+++ tokenize.py
@@ -0,0 +1,23 @@
+from hazm import *
+import os
+import time
+import speech_recognition as sr
+from gtts import gTTS
+import playsound
+
+def get_audio():
+	r = sr.Recognizer()
+	with sr.Microphone() as source :
+		audio = r.listen(source)
+		said = ""
+		try:
+			said = r.recognize_google(audio, language= 'fa-IR')
+			print(said)
+		except Exception as e:
+			print("Exception :" + str(e))
+	return said
+
+
+text = get_audio()
+c=word_tokenize(text)
+print(c)
